e2889c9a82c31ce8978d70499167bf23c6f4db2c
U-Sanzodown\Pierre <sanzodown@hotmail.fr>
2018-05-10 23:29:36 +0200

    handle promises

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
-const request = require('request');
+const rp = require('request-promise');
 const bot = new Discord.Client();
 
 const error404 = "*ISSOU y'a 0 photo pour ta recherche !*";
 const error503 = "*Problème d'api, retry stp*";
+const risibankApi = "https://api.risibank.fr/api/v0/search";
 
 bot.on('ready', function () {
   console.log("Risibot connected !")
@@ -12,7 +13,7 @@ bot.on('ready', function () {
 bot.login('NDQyNzk2OTc2Njc3OTc4MTEy.DdM3Eg._KcG5jUTeJHRd3EDhvHDWIFQYsI');
 
 bot.on('message', message => {
-  
+
   if (message.content === '!risihelp') {
     message.channel.send('**Les commandes disponible pour Risibot :**');
     message.channel.send("*!issou [motclé]*  => Image random de risitas selon le mot clé (seul le premier mot clé est pris en compte atm)");
@@ -21,27 +22,39 @@ bot.on('message', message => {
   if (message.content.startsWith('!issou')){
       let args = message.content.split(' ');
 
-      request.post(
-          'https://api.risibank.fr/api/v0/search',
-          { json: { search: args[1] } },
-          function (error, response, body) {
-              if (!error && response.statusCode === 200) {
-                  if(body.stickers.length > 0) {
-                      let randomPic = Math.floor(Math.random() * Math.floor(body.stickers.length));
+      if (!args[1]) {
+         return message.channel.send("**AYAAA le mec a oublié de mettre un mot-clé**");
+      }
 
-                      if(body.stickers[randomPic].risibank_link) {
-                          message.channel.send("Random Risitas for: "+ args[1], {
-                              file: body.stickers[randomPic].risibank_link
-                          });
-                      } else {
-                          message.channel.send(error404);
-                      }
+      let options = {
+          method: 'POST',
+          uri: risibankApi,
+          body: {
+              search: args[1]
+          },
+          json: true // Automatically stringifies the body to JSON
+      };
+
+      rp(options)
+          .then(function (body) {
+              if(body.stickers.length > 0) {
+                  let randomPic = Math.floor(Math.random() * Math.floor(body.stickers.length));
+
+                  if(body.stickers[randomPic].risibank_link) {
+                      message.channel.send("Random Risitas for: "+ args[1], {
+                          file: body.stickers[randomPic].risibank_link
+                      });
                   } else {
                       message.channel.send(error404);
                   }
+              } else {
+                  message.channel.send(error404);
               }
-          }
-      );
+          })
+          .catch(function (err) {
+              console.log(err);
+              message.channel.send(error503);
+          });
 
   }
 
