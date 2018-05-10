5a97ecfa50348b5834064878a91ec820fd31d6a9
U-Sanzodown\Pierre <sanzodown@hotmail.fr>
2018-05-10 22:19:36 +0200

    init

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const YoutubeStream = require('ytdl-core');
+const request = require('request');
+const bot = new Discord.Client();
+
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+});
+
+bot.login('NDQyNzk2OTc2Njc3OTc4MTEy.DdM3Eg._KcG5jUTeJHRd3EDhvHDWIFQYsI')
+
+bot.on('message', message => {
+  
+  if (message.content === '!risihelp') {
+      message.channel.send('**Les commandes disponible pour Risibot :**');
+    message.channel.send("*!issou [motclé]*  => Image random de risitas selon le mot clé (seul le premier mot clé est pris en compte atm)");
+  }
+
+  if (message.content.startsWith('!issou')){
+      let args = message.content.split(' ');
+
+      request.post(
+          'https://api.risibank.fr/api/v0/search',
+          { json: { search: args[1] } },
+          function (error, response, body) {
+              if (!error && response.statusCode === 200) {
+                  let randomPic = Math.floor(Math.random() * Math.floor(body.stickers.length));
+
+                  message.channel.send("Random Risitas for: "+ args[1], {
+                      file: body.stickers[randomPic].risibank_link
+                  });
+              }
+          }
+      );
+
+  }
+
+});
