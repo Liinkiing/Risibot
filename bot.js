4019a63d6b321afd1da5d7f0863d3646faf3e435
U-Sanzodown\Pierre <sanzodown@hotmail.fr>
2018-05-22 16:54:28 +0200

    add dbd feature

--- bot.js
+++ bot.js
@@ -17,6 +17,18 @@ bot.on('message', message => {
   if (message.content === '!risihelp') {
     message.channel.send('**Les commandes disponible pour Risibot :**');
     message.channel.send("*!issou [motclé]*  => Image random de risitas selon le mot clé (seul le premier mot clé est pris en compte atm)");
+    message.channel.send("*!dbd*  => Désigne une personne sur le voicechan qui sera le tueur");
+  }
+
+  if (message.content === '!dbd'){
+      let voiceChannel = message.member.voiceChannel;
+
+      if(voiceChannel && voiceChannel.members.array().length > 0) {
+          let randomUser = Math.floor(Math.random() * Math.floor(voiceChannel.members.array().length));
+          return message.channel.send("**Le tueur est : **" + voiceChannel.members.array()[randomUser].user.username);
+      }
+
+      return message.channel.send("**AYAAA faut que tu sois dans un voice chan pour pouvoir faire cette commande**");
   }
 
   if (message.content.startsWith('!issou')){
